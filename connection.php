6ba00a74eaef710a168fc8fd9e35e512a2f12593
john-pereira <johnev90@gmail.com>
2022-12-22 16:51:57 +1100

    first commit

---
+++ connection.php
@@ -0,0 +1,24 @@
+<?php  
+
+
+//Paramenters(host, username, password, databasename)
+$conn = mysqli_connect("localhost","root", "", "uploadphp")
+        or die("Could not connect to the database");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
